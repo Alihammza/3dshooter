8b735b6c041b002f0b44c16bf4f978603c30a2b2
VCoves <vcb1234@gmail.com>
2017-10-14 17:55:20 +0200

    shootin arrow

---
+++ Assets/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    Rigidbody rb;
+    Quaternion rot = Quaternion.EulerAngles(-90f,0f,0f);
+        
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(rb.velocity)*rot;
+    }
+}
---
+++ Assets/Instan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instan : MonoBehaviour {
+
+    public GameObject arrowPrefab;
+
+	// Use this for initialization
+	void Start () {
+        //arrowPrefab = Resources.Load("arrow") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (0)) {
+            var go = Instantiate(arrowPrefab, transform.position, transform.rotation);
+        
+            Rigidbody rb = go.GetComponent<Rigidbody>();
+            //  rb.velocity = Camera.main.transform.forward * 30;
+            rb.transform.Translate( new Vector3(8,2,2));
+            rb.velocity = new Vector3(2f, 10f, 8f);
+        }
+	}
+}
